1d35956fb82b582ab23f184f28181c9ea8f406ce
LAIBA ASHRAF <112387588+LAIBAASHRAF@users.noreply.github.com>
2022-10-04 11:47:17 -0700

    Create Series_indexing_slicing.py

---
+++ Series_indexing_slicing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+list_s =pd.Series([1,2,3,4,5,6,7],index =['a','b','c','d','e','f','g'],dtype=int,name="Data_column")
+
+# print(list_s)  -----> print Series
+
+print(list_s['c'])       # print value at index 'c'
+print(list_s['g'])      # print value at index 'g'
+print(list_s['e':'g'])  # Slicing from index 'e' to 'g'   ----> how value at index 'g' is printed it violates the rule of slicing
+print(list_s['a':'g':-1])    # why it displayed empty list
