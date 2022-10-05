d4b0ab15eb7973d1d505070737f49b4228d3d81e
LAIBA ASHRAF <112387588+LAIBAASHRAF@users.noreply.github.com>
2022-10-05 05:30:28 -0700

    Add files via upload

---
+++ Read_csv_file.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+a = pd.read_csv('C:\\Users\\bcm\\Downloads\\TSLA Historical Data.csv')
+print(a)
+b = pd.read_csv('C:\\Users\\bcm\\Downloads\\AABA Historical Data.csv')
+print(b)   # why it show some error
